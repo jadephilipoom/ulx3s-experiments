3ae4acac219f9ed7230baa48feb704480ac29341
Jade Philipoom <jade.philipoom@gmail.com>
2026-06-01 15:53:48 +0200

    remove unnecessary stuff and add a basic state machine

--- top.v
+++ top.v
@@ -1,25 +1,38 @@
 module top(input clk_25mhz,
-           input [6:0] btn,
-           output [7:0] led,
-           output wifi_gpio0);
+           output [7:0] led);
 
     wire i_clk;
     assign i_clk= clk_25mhz;
 
-    // Setting this pin (somehow?) prevents the ESP32 firmware from writing a
-    // passthru bistream and taking over.
-    assign wifi_gpio0 = 1'b1;
-
     reg [7:0] o_led = 0;
-    assign led= o_led;
+    assign led = o_led;
+
+    // Set up basic state machine.
+    localparam STATE_INIT = 2'd0; // Initializing; program not yet loaded.
+    localparam STATE_EXEC = 2'd1; // Executing the program.
+    localparam STATE_DONE = 2'd2; // Program exited (successfully).
+    localparam STATE_ERRS = 2'd3; // Init or exec stages ended due to failures.
+    reg [1:0] state = STATE_INIT;
 
     always @(posedge i_clk) begin
-          if (btn[1]) begin 
-                o_led[7] = 0;
-            end
-          if (btn[2]) begin 
-                o_led[7] = 1;
-            end
+          case (state)
+
+                STATE_INIT: begin
+                    o_led[0] = 1;
+                end
+
+                STATE_EXEC: begin
+                    o_led[1] = 1;
+                end
+
+                STATE_DONE: begin
+                    o_led[2] = 1;
+                end
+
+                STATE_ERRS: begin
+                    o_led[3] = 1;
+                end
+        endcase
     end
 
 endmodule
