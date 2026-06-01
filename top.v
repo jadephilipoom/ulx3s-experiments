004d2d5f7c4e04f3dee681478555c4cf95147cc6
Jade Philipoom <jade.philipoom@gmail.com>
2026-06-01 18:50:17 +0200

    add a reader module

--- top.v
+++ top.v
@@ -1,6 +1,6 @@
 module top(input wire clk_25mhz,
-           output ftdi_rxd,
            input ftdi_txd,
+           output ftdi_rxd,
            output [7:0] led);
 
     assign i_clk = clk_25mhz;
@@ -22,7 +22,7 @@ module top(input wire clk_25mhz,
 
     // Error flags.
     localparam ERRBIT_TIMEOUT = 0; // Maximum cycle count exceeded.
-    localparam ERRBIT_UART = 2;    // Error from the UART module.
+    localparam ERRBIT_SER = 2;     // Error from the serial module.
     localparam ERRBIT_CPU = 1;     // Error from the CPU module.
     localparam ERRBIT_MEM = 2;     // Error from the memory module.
     reg [3:0] errs = 0;
@@ -31,11 +31,25 @@ module top(input wire clk_25mhz,
     reg [63:0] cycle_count = 0;
     localparam TIMEOUT = 64'hffffffff;
 
+    reg [7:0] reader_out;
+    wire reader_valid;
+    wire reader_done;
+    wire reader_err;
+    reader reader(
+        .i_clk(i_clk),
+        .i_en(state == STATE_INIT),
+        .i_tx(ftdi_txd),
+        .o_data(reader_out),
+        .o_valid(reader_valid),
+        .o_done(reader_done),
+        .o_err(reader_err),
+    );
+
     wire cpu_done;
     wire cpu_err;
     cpu cpu(
         .i_clk(i_clk),
-        .i_cpu_en(state == STATE_EXEC),
+        .i_en(state == STATE_EXEC),
         .o_err(cpu_err),
         .o_done(cpu_done),
     );
@@ -47,6 +61,17 @@ module top(input wire clk_25mhz,
 
                 STATE_INIT: begin
                     o_led[1] = 1; // orange led for init
+
+                    // If the serial module is done, transition to the exec state.
+                    if (reader_done) begin
+                        state = STATE_EXEC;
+                    end
+
+                    // If the serial module had errors, transition to the error state.
+                    if (reader_err) begin
+                        errs[ERRBIT_SER] = 1;
+                        state = STATE_ERRS;
+                    end
                 end
 
                 STATE_EXEC: begin
@@ -92,14 +117,49 @@ module top(input wire clk_25mhz,
 
 endmodule
 
+// Reads data from the input serial interface.
+module reader(input wire i_clk,
+              input wire i_en,
+              input wire i_tx,
+              output [7:0] o_data,
+              output wire o_valid,
+              output wire o_done,
+              output wire o_err);
+
+    localparam STATE_WAIT = 2'd0; // Waiting for start sequence.
+    localparam STATE_READ = 2'd1; // Reading data until end sequence.
+    reg [2:0] state = STATE_WAIT;
+
+    always @(posedge i_clk) begin
+        if (i_en) begin
+            o_valid = 0;
+            o_done = 0;
+            case (state)
+
+                 STATE_WAIT: begin
+                     // TODO: listen for start
+                 end
+
+                 STATE_READ: begin
+                     // TODO: listen for end
+                 end
+
+            endcase
+        end
+        else begin
+            o_valid = 0;
+        end
+    end
+
+endmodule
+
 module cpu(input wire i_clk,
-           input wire i_cpu_en,
+           input wire i_en,
            output wire o_err,
            output wire o_done);
 
-    // If cpu-enable is set, execute the program.
     always @(posedge i_clk) begin
-        if (i_cpu_en) begin
+        if (i_en) begin
             // TODO
             o_err = 1;
             o_done = 0;
